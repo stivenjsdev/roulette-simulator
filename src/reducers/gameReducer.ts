7db42fc2e17880cb0cfbeafac01d914f81916c69
Sensei Stiven <strujillodev@gmail.com>
2024-07-07 18:30:36 -0500

    feat: add roulette numbers information

--- src/reducers/gameReducer.ts
+++ src/reducers/gameReducer.ts
@@ -9,7 +9,7 @@ export type GameActions = {
 
 type GameState = {
   patron: number[];
-  gameNumbers: { number: number; tag: string }[];
+  gameNumbers: { number: number; tag: string, color: string }[];
   money: number;
   moneyWin: number;
   moneyLose: number;
@@ -22,6 +22,48 @@ export const initialState: GameState = {
   moneyWin: 0,
   moneyLose: 0,
 };
+
+// const rouletteNumbers = Array.from({ length: 37 }, (_, i) => i);
+const rouletteNumbers: { [key: number]: { color: 'green' | 'red' | 'black' } } = {
+  0: {color: "green"},
+  1: {color: "red"},
+  2: {color: "black"},
+  3: {color: "red"},
+  4: {color: "black"},
+  5: {color: "red"},
+  6: {color: "black"},
+  7: {color: "red"},
+  8: {color: "black"},
+  9: {color: "red"},
+  10: {color: "black"},
+  11: {color: "black"},
+  12: {color: "red"},
+  13: {color: "black"},
+  14: {color: "red"},
+  15: {color: "black"},
+  16: {color: "red"},
+  17: {color: "black"},
+  18: {color: "red"},
+  19: {color: "red"},
+  20: {color: "black"},
+  21: {color: "red"},
+  22: {color: "black"},
+  23: {color: "red"},
+  24: {color: "black"},
+  25: {color: "red"},
+  26: {color: "black"},
+  27: {color: "red"},
+  28: {color: "black"},
+  29: {color: "black"},
+  30: {color: "red"},
+  31: {color: "black"},
+  32: {color: "red"},
+  33: {color: "black"},
+  34: {color: "red"},
+  35: {color: "black"},
+  36: {color: "red"},
+};
+console.log(rouletteNumbers);
 
 export const gameReducer = (state: GameState, action: GameActions) => {
   if (action.type === "ADD_NUMBER_TO_PATRON") {
@@ -44,7 +86,7 @@ export const gameReducer = (state: GameState, action: GameActions) => {
     const tag = state.patron.includes(gameNumber) ? "win" : "lose";
     return {
       ...state,
-      gameNumbers: [...state.gameNumbers, { number: gameNumber, tag }],
+      gameNumbers: [...state.gameNumbers, { number: gameNumber, tag, color: rouletteNumbers[gameNumber].color }],
       money:
         tag === "win"
           ? gameNumber === 32 || gameNumber === 26 || gameNumber === 23
